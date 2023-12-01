81c0a0db1e168714f89e2012cca86b62232e0d17
Ethan <izanperez98@gmail.com>
2023-12-01 23:32:44 +0100

    first commit

---
+++ template.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+day=$1
+mkdir -p ${day}
+template="/*
+*   --- Day X:  ---
+*          --- Part X ---
+*        Advent Of Code 2015
+* */
+
+const lineReader = require('readline').createInterface({
+    input: require('fs').createReadStream('./${day}.txt')
+})
+
+lineReader.on('line', (line) => {
+})
+
+lineReader.on('close', () => {
+    // Result:
+})
+"
+
+echo "$template" > ${day}/${day}_1.js
+echo "$template" > ${day}/${day}_2.js
+touch ${day}/${day}.txt
