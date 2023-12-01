0438db9e0b6760b2e09b457f6fb6dcd8c1070c50
Ethan <izanperez98@gmail.com>
2023-12-01 23:58:42 +0100

    day01 part one

--- day01/day01_1.js
+++ day01/day01_1.js
@@ -8,10 +8,15 @@ const lineReader = require('readline').createInterface({
     input: require('fs').createReadStream('./day01.txt')
 })
 
+let sum = 0
+
 lineReader.on('line', (line) => {
+    const numbers = line.match(/\d/g)
+    sum += parseInt(numbers[0].concat(numbers[numbers.length - 1]))
 })
 
 lineReader.on('close', () => {
-    // Result:
+    console.log('Result:', sum)
+    // Result: 54390
 })
 
