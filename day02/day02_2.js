00177737d51f30b42bf9747292f7962fcd238f56
Ethan <izanperez98@gmail.com>
2023-12-02 12:49:41 +0100

    day02 part two

--- day02/day02_2.js
+++ day02/day02_2.js
@@ -10,36 +10,22 @@ const lineReader = require('readline').createInterface({
 
 let sum = 0
 
-const games = [
-    'Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green',
-    'Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue',
-    'Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red',
-    'Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red',
-    'Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green'
-]
-
-const limit = {
-    red: 12,
-    green: 13,
-    blue: 14
-}
-
-function isPossible(game) {
+function getPower(game) {
     const parts = game.split(': ')
-    const id = parseInt(parts[0].split(' ')[1])
     const cubes = parts[1].split('; ').flatMap(cubes => cubes.split(', '))
-    const isPossible = cubes.every(cube => {
+    const min = { red: 0, green: 0, blue: 0 }
+    cubes.forEach(cube => {
         const [number, color] = cube.split(' ')
-        return parseInt(number) <= limit[color]
+        if (parseInt(number) > min[color]) min[color] = parseInt(number)
     })
-    return isPossible ? id : 0
+    return min.red * min.green * min.blue
 }
 
 lineReader.on('line', (line) => {
-    sum += isPossible(line)
+    sum += getPower(line)
 })
 
 lineReader.on('close', () => {
     console.log('Result:', sum)
-    // Result: 
+    // Result: 71036
 })
