36f3cb60d39a9e117c3b92ecba5d0073f57385d5
Ethan <izanperez98@gmail.com>
2023-12-03 16:36:26 +0100

    day03 init

---
+++ day03/day03_1.js
@@ -0,0 +1,30 @@
+/*
+*   --- Day 3: Gear Ratios ---
+*         --- Part One ---
+*       Advent Of Code 2023
+* */
+
+const lineReader = require('readline').createInterface({
+    input: require('fs').createReadStream('./day03.txt')
+})
+
+const schematic = [
+    '467..114..',
+    '...*......',
+    '..35..633.',
+    '......#...',
+    '617*......',
+    '.....+.58.',
+    '..592.....',
+    '......755.',
+    '...$.*....',
+    '.664.598..'
+]
+
+/*
+lineReader.on('line', (line) => {
+})
+
+lineReader.on('close', () => {
+    // Result:
+})*/
