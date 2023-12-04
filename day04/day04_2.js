226df47208a5bd6143a9eb4d20b02838aa2f78d1
Ethan <izanperez98@gmail.com>
2023-12-04 17:05:04 +0100

    day04 part two

--- day04/day04_2.js
+++ day04/day04_2.js
@@ -8,15 +8,21 @@ const lineReader = require('readline').createInterface({
     input: require('fs').createReadStream('./day04.txt')
 })
 
-let total = 0
+let n = 0
+const cards = new Array(212).fill(1)
 
 lineReader.on('line', (line) => {
     line = line.split(': ')
     const [wins, haves] = line[1].split(' | ').map(str => str.split(' ').flatMap(val => parseInt(val)).filter(val => !isNaN(val)))
-    total += haves.reduce((acc, have) => wins.includes(have) ? ((acc > 0) ? acc *= 2 : ++acc) : acc, 0)
+    const matches = haves.reduce((acc, have) => wins.includes(have) ? ++acc : acc, 0)
+    const currentCard = n++
+    for (let i = currentCard + 1; i < cards.length && i < currentCard + matches + 1; i++) {
+        cards[i] += cards[currentCard]
+    }
 })
 
 lineReader.on('close', () => {
-    console.log('Result', total)
-    // Result: 
+    const res = cards.reduce((acc, card) => acc + card, 0)
+    console.log('Result', res)
+    // Result: 9924412
 })
