67e326d40292f43c818cc4d91721a6d660439778
Ethan <izanperez98@gmail.com>
2023-12-04 13:09:07 +0100

    day04 part one

--- day04/day04_1.js
+++ day04/day04_1.js
@@ -8,9 +8,15 @@ const lineReader = require('readline').createInterface({
     input: require('fs').createReadStream('./day04.txt')
 })
 
+let total = 0
+
 lineReader.on('line', (line) => {
+    line = line.split(': ')
+    const [wins, haves] = line[1].split(' | ').map(str => str.split(' ').flatMap(val => parseInt(val)).filter(val => !isNaN(val)))
+    total += haves.reduce((acc, have) => wins.includes(have) ? ((acc > 0) ? acc *= 2 : ++acc) : acc, 0)
 })
 
 lineReader.on('close', () => {
-    // Result:
+    console.log('Result', total)
+    // Result: 25010
 })
--- day04/day04_2.js
+++ day04/day04_2.js
@@ -1,16 +1,22 @@
 /*
-*   --- Day X:  ---
-*          --- Part X ---
-*        Advent Of Code 2023
+*   --- Day 4: Scratchcards ---
+*        --- Part Two ---
+*       Advent Of Code 2023
 * */
 
 const lineReader = require('readline').createInterface({
     input: require('fs').createReadStream('./day04.txt')
 })
 
+let total = 0
+
 lineReader.on('line', (line) => {
+    line = line.split(': ')
+    const [wins, haves] = line[1].split(' | ').map(str => str.split(' ').flatMap(val => parseInt(val)).filter(val => !isNaN(val)))
+    total += haves.reduce((acc, have) => wins.includes(have) ? ((acc > 0) ? acc *= 2 : ++acc) : acc, 0)
 })
 
 lineReader.on('close', () => {
-    // Result:
+    console.log('Result', total)
+    // Result: 
 })
