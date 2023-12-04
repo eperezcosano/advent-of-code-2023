23f10058baa502c06b986cdcbf989b3491c056e7
Ethan <izanperez98@gmail.com>
2023-12-04 12:36:20 +0100

    day04 init

---
+++ day04/day04_1.js
@@ -0,0 +1,16 @@
+/*
+*   --- Day 4: Scratchcards ---
+*        --- Part One ---
+*       Advent Of Code 2023
+* */
+
+const lineReader = require('readline').createInterface({
+    input: require('fs').createReadStream('./day04.txt')
+})
+
+lineReader.on('line', (line) => {
+})
+
+lineReader.on('close', () => {
+    // Result:
+})
