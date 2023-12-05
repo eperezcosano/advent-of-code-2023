f51d7c80942e2b33789a219a9fdd5b4eaed4ce81
Ethan <izanperez98@gmail.com>
2023-12-05 15:53:00 +0100

    day05 part one

---
+++ day05/day05_1.js
@@ -0,0 +1,48 @@
+/*
+*   --- Day 5: If You Give A Seed A Fertilizer ---
+*                   --- Part One ---
+*                 Advent Of Code 2023
+* */
+
+const lineReader = require('readline').createInterface({
+    input: require('fs').createReadStream('./day05.txt')
+})
+
+let seeds = []
+const maps = [[], [], [], [], [], [], []]
+
+function inRange(src, dest, range) {
+    return src >= dest && src < dest + range
+}
+
+function convert(seeds, map) {
+    for (let i = 0; i < seeds.length; i++) {
+        for (const [dest, src, range] of map) {
+            if (inRange(seeds[i], src, range)) {
+                seeds[i] -= src - dest
+                break
+            }
+        }
+    }
+}
+
+let storeMap = false
+let group = -1
+lineReader.on('line', (line) => {
+    if (line.length == 0) {
+        storeMap = false
+        group++
+    }
+    else if (storeMap) maps[group].push(line.split(' ').map(val => parseInt(val)))
+    else if (line.startsWith('seeds: ')) seeds = line.split(': ')[1].split(' ').map(val => parseInt(val))
+    else if (line.endsWith('map:')) storeMap = true
+})
+
+lineReader.on('close', () => {
+    for (const map of maps) {
+        convert(seeds, map)
+    }
+    const res = Math.min(...seeds)
+    console.log('Result:', res)
+    // Result: 84470622
+})
