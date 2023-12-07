02ef747137aa735ae189fedd6b568425c60e17af
Ethan <izanperez98@gmail.com>
2023-12-07 13:06:10 +0100

    day07 init

---
+++ day07/day07_1.js
@@ -0,0 +1,16 @@
+/*
+*   --- Day 7: Camel Cards ---
+*        --- Part One ---
+*       Advent Of Code 2023
+* */
+
+const lineReader = require('readline').createInterface({
+    input: require('fs').createReadStream('./day07.txt')
+})
+
+lineReader.on('line', (line) => {
+})
+
+lineReader.on('close', () => {
+    // Result:
+})
