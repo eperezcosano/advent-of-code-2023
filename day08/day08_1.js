3b8c66cb3208993b64f201235e606ca430d44ce5
Ethan <izanperez98@gmail.com>
2023-12-08 14:59:14 +0100

    day08 part one

--- day08/day08_1.js
+++ day08/day08_1.js
@@ -8,9 +8,31 @@ const lineReader = require('readline').createInterface({
     input: require('fs').createReadStream('./day08.txt')
 })
 
+const instructions = []
+const network = new Map()
+
+function walk() {
+    let node = 'AAA'
+    for (let i = 0; ; i++) {
+        const [left, right] = network.get(node)
+        node = instructions[i % instructions.length] === 'L' ? left : right
+        if (node === 'ZZZ') return i + 1
+    }
+}
+
+let n = 0
 lineReader.on('line', (line) => {
+    if (n == 0) instructions.push(...line.split(''))
+    if (n >= 2) {
+        const [node, dest] = line.split(' = ')
+        const [left, right] = dest.split(', ').map(val => val.replace('(', '').replace(')', ''))
+        network.set(node, [left, right])
+    }
+    n++
 })
 
 lineReader.on('close', () => {
-    // Result:
+    const res = walk()
+    console.log('Result:', res)
+    // Result: 20093
 })
