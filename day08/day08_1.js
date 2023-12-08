fc4116f37d50093eb8e1b106e927c1f2e624d8b1
Ethan <izanperez98@gmail.com>
2023-12-08 14:37:19 +0100

    day08 init

---
+++ day08/day08_1.js
@@ -0,0 +1,16 @@
+/*
+*   --- Day 8: Haunted Wasteland ---
+*          --- Part One ---
+*        Advent Of Code 2023
+* */
+
+const lineReader = require('readline').createInterface({
+    input: require('fs').createReadStream('./day08.txt')
+})
+
+lineReader.on('line', (line) => {
+})
+
+lineReader.on('close', () => {
+    // Result:
+})
