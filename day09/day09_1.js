f7da85e3c7fe4b48f070ffc5a8d84e32625ef321
Ethan <izanperez98@gmail.com>
2023-12-09 12:58:12 +0100

    day09 part one

--- day09/day09_1.js
+++ day09/day09_1.js
@@ -8,9 +8,23 @@ const lineReader = require('readline').createInterface({
     input: require('fs').createReadStream('./day09.txt')
 })
 
+const sequences = []
+
+function extrapolate(sequence) {
+    const res = []
+    for (let i = 1; i < sequence.length; i++) {
+        res.push(sequence[i] - sequence[i - 1])
+    }
+    if (res.every(num => num == 0)) return 0
+    return extrapolate(res) + res[res.length - 1]
+}
+
 lineReader.on('line', (line) => {
+    sequences.push(line.split(' ').map(val => parseInt(val)))
 })
 
-lineReader.on('close', () => {
-    // Result:
+lineReader.on('close', () => {    
+    const res = sequences.reduce((acc, sequence) => acc + extrapolate(sequence) + sequence[sequence.length - 1], 0)
+    console.log('Result:', res)
+    // Result: 1842168671
 })
--- day09/day09_2.js
+++ day09/day09_2.js
@@ -1,16 +1,30 @@
 /*
-*   --- Day X:  ---
-*          --- Part X ---
-*        Advent Of Code 2023
+*   --- Day 9: Mirage Maintenance ---
+*           --- Part Two ---
+*         Advent Of Code 2023
 * */
 
 const lineReader = require('readline').createInterface({
-    input: require('fs').createReadStream('./day09.txt')
+    input: require('fs').createReadStream('./day09_example.txt')
 })
+
+const sequences = []
+
+function extrapolate(sequence) {
+    const res = []
+    for (let i = 1; i < sequence.length; i++) {
+        res.push(sequence[i] - sequence[i - 1])
+    }
+    if (res.every(num => num == 0)) return 0
+    return extrapolate(res) + res[res.length - 1]
+}
 
 lineReader.on('line', (line) => {
+    sequences.push(line.split(' ').map(val => parseInt(val)))
 })
 
-lineReader.on('close', () => {
-    // Result:
+lineReader.on('close', () => {    
+    const res = sequences.reduce((acc, sequence) => acc + extrapolate(sequence) + sequence[sequence.length - 1], 0)
+    console.log('Result:', res)
+    // Result: 
 })
