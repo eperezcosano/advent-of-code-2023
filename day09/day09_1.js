5f9956dbb5d3eb2210f48921a1c8fb125e626cf0
Ethan <izanperez98@gmail.com>
2023-12-09 12:00:43 +0100

    day09 init

---
+++ day09/day09_1.js
@@ -0,0 +1,16 @@
+/*
+*   --- Day 9: Mirage Maintenance ---
+*           --- Part One ---
+*         Advent Of Code 2023
+* */
+
+const lineReader = require('readline').createInterface({
+    input: require('fs').createReadStream('./day09.txt')
+})
+
+lineReader.on('line', (line) => {
+})
+
+lineReader.on('close', () => {
+    // Result:
+})
