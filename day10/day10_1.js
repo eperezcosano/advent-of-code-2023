7775086496e35eb843a9c12de809ffaa062d21e4
Ethan <izanperez98@gmail.com>
2023-12-11 18:01:58 +0100

    day10 init

---
+++ day10/day10_1.js
@@ -0,0 +1,16 @@
+/*
+*   --- Day 10: Pipe Maze ---
+*       --- Part One ---
+*     Advent Of Code 2023
+* */
+
+const lineReader = require('readline').createInterface({
+    input: require('fs').createReadStream('./day10.txt')
+})
+
+lineReader.on('line', (line) => {
+})
+
+lineReader.on('close', () => {
+    // Result:
+})
