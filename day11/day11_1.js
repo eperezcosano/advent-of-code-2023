0c0155f112a3a973e4f4416d10b09120ec703e76
Ethan <izanperez98@gmail.com>
2023-12-17 19:29:37 +0100

    day11 init

---
+++ day11/day11_1.js
@@ -0,0 +1,16 @@
+/*
+*   --- Day 11: Cosmic Expansion ---
+*          --- Part One ---
+*         Advent Of Code 2023
+* */
+
+const lineReader = require('readline').createInterface({
+    input: require('fs').createReadStream('./day11.txt')
+})
+
+lineReader.on('line', (line) => {
+})
+
+lineReader.on('close', () => {
+    // Result:
+})
---
+++ day11/day11_2.js
@@ -0,0 +1,16 @@
+/*
+*   --- Day X:  ---
+*          --- Part X ---
+*        Advent Of Code 2023
+* */
+
+const lineReader = require('readline').createInterface({
+    input: require('fs').createReadStream('./day11.txt')
+})
+
+lineReader.on('line', (line) => {
+})
+
+lineReader.on('close', () => {
+    // Result:
+})
