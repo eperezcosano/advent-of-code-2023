4e14f90ec8b5511286ddc0c55959c2cd83dbce57
Ethan <izanperez98@gmail.com>
2023-12-20 20:53:06 +0100

    day12 part two

--- day12/day12_2.js
+++ day12/day12_2.js
@@ -8,9 +8,13 @@ const lineReader = require('readline').createInterface({
     input: require('fs').createReadStream('./day12.txt')
 })
 
+const cache = new Map()
+
 function count(string, nums) {
     if (string == "") return nums.length == 0 ? 1 : 0
     if (nums.length == 0) return string.includes('#') ? 0 : 1
+
+    if (cache.has([string, nums].join('-'))) return cache.get([string, nums].join('-'))
     let result = 0
     if (string[0] == '.' || string[0] == '?') result += count(string.slice(1), nums)
     if (string[0] == '#' || string[0] == '?') {
@@ -18,6 +22,7 @@ function count(string, nums) {
             result += count(string.slice(nums[0] + 1), nums.slice(1))
         }
     }
+    cache.set([string, nums].join('-'), result)
     return result
 }
 
@@ -25,10 +30,10 @@ let sum = 0
 
 lineReader.on('line', (line) => {
     const [string, nums] = line.split(' ').map((val, i) => i == 1 ? val.split(',').map(num => parseInt(num)) : val)
-    sum += count(string.slice(), nums.slice())
+    sum += count(Array(5).fill(string).join('?'), Array(5).fill(nums).flat())
 })
 
 lineReader.on('close', () => {
     console.log('Result:', sum)
-    // Result: 7857
+    // Result: 28606137449920
 })
