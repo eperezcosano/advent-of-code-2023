093746854a77ce259ab77b1368ed0a25d3f98628
Ethan <izanperez98@gmail.com>
2023-12-20 20:43:44 +0100

    day12 part one

--- day12/day12_1.js
+++ day12/day12_1.js
@@ -8,9 +8,27 @@ const lineReader = require('readline').createInterface({
     input: require('fs').createReadStream('./day12.txt')
 })
 
+function count(string, nums) {
+    if (string == "") return nums.length == 0 ? 1 : 0
+    if (nums.length == 0) return string.includes('#') ? 0 : 1
+    let result = 0
+    if (string[0] == '.' || string[0] == '?') result += count(string.slice(1), nums)
+    if (string[0] == '#' || string[0] == '?') {
+        if (nums[0] <= string.length && !string.slice(0, nums[0]).includes('.') && (nums[0] == string.length || string[nums[0]] != '#')) {
+            result += count(string.slice(nums[0] + 1), nums.slice(1))
+        }
+    }
+    return result
+}
+
+let sum = 0
+
 lineReader.on('line', (line) => {
+    const [string, nums] = line.split(' ').map((val, i) => i == 1 ? val.split(',').map(num => parseInt(num)) : val)
+    sum += count(string.slice(), nums.slice())
 })
 
 lineReader.on('close', () => {
-    // Result:
+    console.log('Result:', sum)
+    // Result: 7857
 })
