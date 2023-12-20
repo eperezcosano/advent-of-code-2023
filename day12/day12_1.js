a554774beab580e448b82f81b29d3c9a0179a2c9
Ethan <izanperez98@gmail.com>
2023-12-20 14:14:41 +0100

    day12 init

---
+++ day12/day12_1.js
@@ -0,0 +1,16 @@
+/*
+*   --- Day 12: Hot Springs ---
+*        --- Part One ---
+*       Advent Of Code 2023
+* */
+
+const lineReader = require('readline').createInterface({
+    input: require('fs').createReadStream('./day12.txt')
+})
+
+lineReader.on('line', (line) => {
+})
+
+lineReader.on('close', () => {
+    // Result:
+})
