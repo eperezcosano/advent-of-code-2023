04fee3c737f5eb61edd926aaaf88081cb240dc3f
Ethan <izanperez98@gmail.com>
2024-02-19 14:36:54 +0100

    day14 init

---
+++ day14/day14_1.js
@@ -0,0 +1,16 @@
+/*
+*   --- Day 14: Parabolic Reflector Dish ---
+*                --- Part One ---
+*              Advent Of Code 2023
+* */
+
+const lineReader = require('readline').createInterface({
+    input: require('fs').createReadStream('./day14.txt')
+})
+
+lineReader.on('line', (line) => {
+})
+
+lineReader.on('close', () => {
+    // Result:
+})
