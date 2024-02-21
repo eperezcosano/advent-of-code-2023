08ecce72e834ab62722800fc153d7e8964a18751
Ethan <izanperez98@gmail.com>
2024-02-21 16:35:20 +0100

    day15 part one

--- day15/day15_1.js
+++ day15/day15_1.js
@@ -5,12 +5,17 @@
 * */
 
 const lineReader = require('readline').createInterface({
-    input: require('fs').createReadStream('./day15.txt')
-})
+  input: require('fs').createReadStream('./day15.txt'),
+});
+
+function hash(str) {
+  return str
+    .split('')
+    .reduce((acc, _, i) => ((acc + str.charCodeAt(i)) * 17) % 256, 0);
+}
 
 lineReader.on('line', (line) => {
-})
-
-lineReader.on('close', () => {
-    // Result:
-})
+  const res = line.split(',').reduce((acc, str) => acc + hash(str), 0);
+  console.log('Result:', res);
+  // Result: 519041
+});
