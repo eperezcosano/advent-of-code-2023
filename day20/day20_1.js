1d6aa8d32a780f026db71340295a0144c80e0520
Ethan <izanperez98@gmail.com>
2024-03-22 16:20:35 +0100

    day20 init

---
+++ day20/day20_1.js
@@ -0,0 +1,16 @@
+/*
+*   --- Day 20: Pulse Propagation ---
+*           --- Part One ---
+*          Advent Of Code 2023
+* */
+
+const lineReader = require('readline').createInterface({
+    input: require('fs').createReadStream('./day20.txt')
+})
+
+lineReader.on('line', (line) => {
+})
+
+lineReader.on('close', () => {
+    // Result:
+})
