59838a9c86abcb10e6399be3fe772dc3a084967d
Ethan <izanperez98@gmail.com>
2024-05-02 13:12:16 +0200

    day25 init

---
+++ day25/day25.js
@@ -0,0 +1,15 @@
+/*
+*   --- Day 25: Snowverload ---
+*       Advent Of Code 2023
+* */
+
+const lineReader = require('readline').createInterface({
+    input: require('fs').createReadStream('./example.txt')
+})
+
+lineReader.on('line', (line) => {
+})
+
+lineReader.on('close', () => {
+    // Result:
+})
